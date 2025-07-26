641040f5d89aff164be4c2459075ada6132f2279
Fabrizio-Jorgge <fabriziomaldonado7@gmail.com>
2025-07-26 13:31:29 +0000

    hola desde la unida

---
+++ Clase 4/app.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+#caracteres especiales
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+
+def unida()
+    return 'Hola desde la UNIDA'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
+    
