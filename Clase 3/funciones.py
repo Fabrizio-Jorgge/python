6553fadfbe99a5e2eec60b521fea9dba1d16ccb9
Fabrizio-Jorgge <fabriziomaldonado7@gmail.com>
2025-07-26 11:53:32 +0000

    funciones

---
+++ Clase 3/funciones.py
@@ -0,0 +1,15 @@
+
+
+def sumar (a, b):
+    """Suma dos números y devuelve el resultado."""
+    return a + b
+
+resultado = sumar(5, 3)
+print("resultado de la suma:", resultado)
+
+def restar(a, b):
+    """Resta dos números y devuelve el resultado."""
+    return a - b
+
+resultado_resta = restar(10, 4)
+print("resultado de la resta:", resultado_resta)    
