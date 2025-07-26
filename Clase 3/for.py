eaf43f4609ec0534259abb0b3d1779d2d7fbbe18
Fabrizio-Jorgge <fabriziomaldonado7@gmail.com>
2025-07-26 06:10:45 +0000

    for

---
+++ Clase 3/for.py
@@ -0,0 +1,10 @@
+'''
+Ejemplo estructura de control FOR
+'''
+
+#Ejemplo con una lista
+array=["Futbol","pc",18.6,18,[6,7,10.4],True,False]
+
+for i in range(len(array)):
+    print("Elemento en la posición", i, ":", array[i])  
+print("Bucle terminado")  # Mensaje al finalizar el bucle
