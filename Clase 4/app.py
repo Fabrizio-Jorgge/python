38e7f957fc0551190ff7bf87892258ffc8afdd78
Fabrizio-Jorgge <fabriziomaldonado7@gmail.com>
2025-07-26 13:34:06 +0000

    hola desde la UNIDA

--- Clase 4/app.py
+++ Clase 4/app.py
@@ -7,9 +7,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 
-def unida()
+def unida():
     return 'Hola desde la UNIDA'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
-    
