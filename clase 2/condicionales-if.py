5bba2c70d14a73daccc1c70a9a3aec711d6db3f2
Fabrizio-Jorgge <fabriziomaldonado7@gmail.com>
2025-07-26 05:06:03 +0000

    condicionales-if

---
+++ clase 2/condicionales-if.py
@@ -0,0 +1,30 @@
+'''
+condicionales if
+''' 
+
+dato =int(input("Ingrese un dato: "))
+if dato > 0: 
+    print("El dato es positivo")
+elif dato < 0:
+    print("El dato es negativo")
+else:
+    print("El dato es cero")
+    
+#un if
+if dato % 2 == 0:
+    print("El dato es par")
+
+#if else 
+if dato % 2 == 0:
+    print("El dato es par")
+else:
+    print("El dato es impar")
+
+#if else if
+if dato > 0:
+    print("El dato es positivo")
+elif dato < 0:
+    print("El dato es negativo")
+else:
+    print("El dato es cero")
+    
