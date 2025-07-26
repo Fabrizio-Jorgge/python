ee76c221c0656826d22821f027f3d8c62d9b3167
Fabrizio-Jorgge <fabriziomaldonado7@gmail.com>
2025-07-26 04:31:49 +0000

    finalizado operadores

---
+++ clase 2/operadores.py
@@ -0,0 +1,22 @@
+'''
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+#Operadores de comparacion
+'''
+
+r=100 == 100
+print(r)
+r= 100 != 100
+print(r)
+r= 100 > 100   
+print(r)
+r= 100 < 100
+print(r)
+r= 100 >= 100
+print(r)
+r= 100 <= 100
+print(r)
