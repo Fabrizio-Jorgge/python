ef914075708fdf0dceba33e137444d9dc4482f6f
Fabrizio-Jorgge <fabriziomaldonado7@gmail.com>
2025-07-26 04:04:06 +0000

    operaciones

--- clase 2/variables.py
+++ clase 2/variables.py
@@ -1,1 +1,19 @@
 print("hello world from UNIDA")
+entero = 10
+flotante = 10.5
+cadena = "hola"
+booleano = True
+print(entero, flotante, cadena, booleano)
+print(type(entero), type(flotante), type(cadena), type(booleano))
+
+
+print("sumando dos numeros")
+a=10
+b=20
+print("suma de dos numeros", a+b)
+print("restando dos numeros")
+print("resta de dos numeros", a-b)
+print("multiplicando dos numeros") 
+print("multiplicacion de dos numeros", a*b) 
+print("dividiendo dos numeros") 
+print("division de dos numeros", a/b)
